96cc3d99e7cdec3a287eb7d93af4d0a3fef7391a
ryu57sp <ryuki.baske5@gmail.com>
2022-04-30 02:11:02 +0900

    ToDoList

--- config/todo/models.py
+++ config/todo/models.py
@@ -1,3 +1,13 @@
+from pydoc import describe
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField("やること", max_length=30)
+    description = models.TextField("詳細", blank=True)
+    deadline = models.DateField("締め切り")
+
+    def __str__(self):
+        return self.title
